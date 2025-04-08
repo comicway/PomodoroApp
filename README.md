6118170ac153e57b8ab12885d535ac56caa50804
Moises Contreras <129238200+comicway@users.noreply.github.com>
2025-04-08 10:52:37 -0400

    Instalacion de TailWindCss
    Instalacion de TailWindCss y cambios en el archivo README

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # PomodoroApp
+
  Aplicación personalizada basado en el método de concentración Pomodoro
 
 Mira mi pagina web: 
 
 [https://www.moises-script.cl/](https://www.moises-script.cl/)
+
+## Funcionalidades PMV
+
+- Añadir sonido de notificación usando react-use
+- Registro de sesiones completadas
+- List To Do vinculadas a las sesiones
--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@ import PomodoroTimer from './components/PomodoroTimer';
 
 function App() {
   return (
-    <div className="app">
-      <h1>Pomodoro Timer</h1>
-      <PomodoroTimer />
-    </div>
+    <>
+        <div className="container mx-auto">
+          <div className='grid grid-cols-1 sm:grid-cols-4'>
+            <h1>Pomodoro Timer</h1>
+            <PomodoroTimer />
+          </div>
+        </div>
+    </>
   );
 }
 
