500db77da7008ad130851dc7f36a143832e6e160
comicway <mises.xiaomi@gmail.com>
2025-07-01 06:05:07 -0400

    probandoCommit

--- src/components/PomodoroTimer.jsx
+++ src/components/PomodoroTimer.jsx
@@ -9,7 +9,9 @@ const PomodoroTimer = () => {
   const [key, setKey] = useState(0); // Para reiniciar el temporizador
   const [tasks, setTasks] = useState([]);
   const [completePomodoro, setCompletePomodoro] = useState(0);
-  
+
+  console.log('El pomodoro esta activo o no:', isPlaying);
+
   // Duración en segundos (25 minutos)
   const POMODORO_DURATION = 1 * 60;
 
@@ -40,7 +42,7 @@ const PomodoroTimer = () => {
   };
 
   const handleTaskCompletion = (indexToComplete) => {
-    setTasks(tasks.map((task, index) => 
+    setTasks(tasks.map((task, index) =>
       index === indexToComplete ? { ...task, completed: !task.completed } : task
     ));
   };
@@ -49,13 +51,13 @@ const PomodoroTimer = () => {
     <>
       <div className="container">
         <div className='grid grid-cols-1 gap-5'>
-          <h1 className='mx-auto'>Pom odoro Timer</h1>
+          <h1 className='mx-auto'>Pomodoro Timer</h1>
           <CountdownCircleTimer
             key={key}
             isPlaying={isPlaying}
             duration={POMODORO_DURATION}
             colors={['#00ff00', '#ffff00', '#ff0000']}
-            colorsTime={[POMODORO_DURATION, POMODORO_DURATION/2, 0]}
+            colorsTime={[POMODORO_DURATION, POMODORO_DURATION / 2, 0]}
             onComplete={handleComplete}
             size={300}
             strokeWidth={15}
@@ -70,7 +72,7 @@ const PomodoroTimer = () => {
                   <button onClick={() => {
                     setKey(prev => prev + 1);
                     setIsPlaying(false);
-                  }}>
+                  }} style={{ isPlaying ? display: 'none' : 'block' }}>
                     Reiniciar
                   </button>
                 </div>
